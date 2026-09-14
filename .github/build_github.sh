fecb5d1f67c1f8222e7dfd59ad675502649cf90a
oexi <83975611+oexi@users.noreply.github.com>
2026-09-14 15:49:32 +0800

    build: use SideStore 0.7.0-alpha

--- .github/build_github.sh
+++ .github/build_github.sh
@@ -41,7 +41,7 @@ mv ./tmp/SideStoreSupport.framework Payload/LiveContainer.app/Frameworks
 
 # download SideStore
 cd tmp
-wget https://github.com/SideStore/SideStore/releases/download/nightly/SideStore.ipa
+wget https://github.com/SideStore/SideStore/releases/download/alpha/SideStore.ipa
 unzip SideStore.ipa
 cd ..
 
